e33f5782ae517fbc645c7b63f3e1d61d9db6ca7e
sdsuy <santiagodasilva@sdsuy.dev>
2026-03-13 17:55:55 -0300

    commit 8: add blade layout

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,13 +1,17 @@
-<h1>Lista de tareas</h1>
+@extends('layouts.app')
+
+@section('content')
+
+<h2>Lista de tareas</h2>
 
 <ul>
 
 @foreach($tasks as $task)
 
-<li>
-{{ $task->title }}
-</li>
+<li>{{ $task->title }}</li>
 
 @endforeach
 
 </ul>
+
+@endsection
