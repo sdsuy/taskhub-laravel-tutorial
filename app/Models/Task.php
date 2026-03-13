f860361de6faafdc53f92811f72855251512a536
sdsuy <santiagodasilva@sdsuy.dev>
2026-03-13 17:45:00 -0300

    commit 4: configure task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,7 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Modelo Task
+ *
+ * Representa una fila en la tabla tasks.
+ */
 class Task extends Model
 {
-    //
+    /**
+     * Mass assignment
+     *
+     * Permite asignar estos campos en masa.
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'completed'
+    ];
 }
