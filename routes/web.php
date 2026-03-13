ea1c411acf0fe2e4461240ee853af27446d10904
sdsuy <santiagodasilva@sdsuy.dev>
2026-03-13 17:49:15 -0300

    commit 6: add tasks route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| Task routes
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', [TaskController::class, 'index']);
